a04d023726ee904515d30b3a136867da7bfac80e
Kye <kdavis6911@bths.edu>
2018-09-18 10:15:31 -0400

    End of class update

--- src/KDavis/KDavisLib.java
+++ src/KDavis/KDavisLib.java
@@ -6,4 +6,20 @@ public class KDavisLib
     {
         System.out.println(str);
     }
+    public static boolean isPalindrome(String word)
+    {
+        String rev = "";
+        for (int i = word.length() - 1; i >= 0; i--)
+        {
+            rev += word.substring(i,i + 1);
+        }
+        if (rev.equals(word))
+        {
+            return true;
+        }
+        return false;
+    }
+    public static void foobarbaz(int numberofnumbers)
+    {
+        for(int i = 1; i <= numberofnumbers; i++)
 }
--- src/KDavis/TestFile.java
+++ src/KDavis/TestFile.java
@@ -1,11 +1,14 @@
 package KDavis;
 
+import static KDavis.KDavisLib.isPalindrome;
 import static KDavis.KDavisLib.println;
 
 public class TestFile
 {
     public static void main(String[] args)
     {
-        
+        boolean test = isPalindrome("tweakin");
+        System.out.print(test);
     }
 }
+
