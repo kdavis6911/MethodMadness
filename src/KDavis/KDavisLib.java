cfab90e829a89e61f64a28b0a71934f7dcb478da
Kye <kdavis6911@bths.edu>
2018-09-21 10:20:05 -0400

    Version 3

--- src/KDavis/KDavisLib.java
+++ src/KDavis/KDavisLib.java
@@ -9,6 +9,13 @@ public class KDavisLib
     {
         System.out.println(str);
     }
+
+    /**
+     * isPalindrome will check if param "word" is the same backwards as it is forward
+     * param "rev" is the word backwards
+     * Returns a boolean
+     */
+
     public static boolean isPalindrome(String word)
     {
         String rev = "";
@@ -22,6 +29,14 @@ public class KDavisLib
         }
         return false;
     }
+
+    /**
+     * cutOut takes the first instance of your substring out of your mainstring
+     * param "mainstr" is the long word you're cutting out from
+     * param "substr" is the phrase you're cutting out from mainstr
+     * Returns mainstr with substr cut out
+     */
+
     public static String cutOut(String mainstr, String substr)
     {
         int substrplace = mainstr.indexOf(substr);
@@ -33,8 +48,17 @@ public class KDavisLib
         String nextshortstr = mainstr.substring(substrplace + substr.length());
         return firstshortstr + nextshortstr;
     }
-    public static void multiplicationTable(int base, int range) {
-        if (range <= 0) {
+
+    /**
+     * multiplicationTable multiplies all numbers between 0 and param "range" multiplied by param "base" separately
+     * param "base" is the number you're multiplying the sequence by
+     * param "range" is where the sequence ends
+     */
+
+    public static void multiplicationTable(int base, int range)
+    {
+        if (range <= 0)
+        {
             System.out.println("I can't do that!");
         }
         String table = "";
@@ -44,13 +68,29 @@ public class KDavisLib
         }
         System.out.println(table);
     }
+
+    /**
+     * sumUpTo returns the sum of all numbers between 0 and param "num"
+     * param "num" is the limit
+     * Returns an int which is the sum of all the numbers
+     */
+
     public static int sumUpTo(int num)
     {
-        int sumofall = 0
+        int sumofall = 0;
         for (int i = 0; i <= num; i++)
         {
-            sumofall + i;
+            sumofall = sumofall + i;
         }
-        return sumofall
+        return sumofall;
+    }
+    public static int leastCommonMultiple(int num1, int num2, int num3)
+    {
+        for (int i = 0; i < 0; i++)
+        {
+            if (i % num1 == 0 && i % num2 == 0 && i % num3 == 0);
+            return i;
+        }
+        return -1;
     }
 }
--- src/KDavis/TestFile.java
+++ src/KDavis/TestFile.java
@@ -9,15 +9,24 @@ public class TestFile
 {
     public static void main(String[] args)
     {
+        //test for isPalindrome
         boolean test = isPalindrome("tweakin");
         System.out.println(test);
 
-        String test1 = cutOut("", "");
+        //test for cutOut
+        String test1 = cutOut("young thugga la flare", "la");
         System.out.println(test1);
 
-        multiplicationTable(8, -4);
+        //test for multiplicationTable
+        multiplicationTable(8, 12);
 
+        //test for sumUpTo
+        int test2 = sumUpTo(24);
+        System.out.println(test2);
 
+        //test for leastCommonMultiple
+        int test3 = leastCommonMultiple(2,4,6);
+        System.out.println(test3);
     }
 }
 
